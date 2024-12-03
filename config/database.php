02c643da2e5f98568143c560e238057469e9beea
trannam1303 <tranhoainam29012002@gmail.com>
2024-12-03 10:53:40 +0700

    fix connectDB

--- config/database.php
+++ config/database.php
@@ -1,19 +1,21 @@
 <?php
-$severname = "localhost";
-$username = "root";
-$password = "";
-$database = "example_crud";
-
-$conn = mysqli_connect($severname,$username,$password,$database);
-
-if (!$conn) {
-    // If connection fails, display the error
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully to the database!";
+class Database {
+    private $severname = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "example_crud";
+    public $conn;
+    
+    public function mysqli_connect(): ?PDO{    
+        $this->conn = null;    
+        try {        
+            $this->conn = new PDO("mysql:host=" . $this->severname . ";dbname=" . $this->database, $this->username, $this->password);        
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+        } catch (PDOException $exception) {        
+            echo "Connection error: " . $exception->getMessage();    
+        }    
+        return $this->conn;
+    }
 }
-
-// Close connection
-mysqli_close($conn);
 ?>
 
