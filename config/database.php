c63793a7df5057f1fd4ec9d1ce5a344de74ecd96
trannam1303 <tranhoainam29012002@gmail.com>
2024-12-03 09:27:25 +0700

    config database

--- config/database.php
+++ config/database.php
@@ -1,1 +1,19 @@
 <?php
+$severname = "localhost";
+$username = "root";
+$password = "";
+$database = "example_crud";
+
+$conn = mysqli_connect($severname,$username,$password,$database);
+
+if (!$conn) {
+    // If connection fails, display the error
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully to the database!";
+}
+
+// Close connection
+mysqli_close($conn);
+?>
+
