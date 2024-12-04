0b0d635cd0936a6878d41ed7d0879b70efebe985
trannam1303 <tranhoainam29012002@gmail.com>
2024-12-04 10:52:04 +0700

    list usser

--- app/controllers/UserControllers.php
+++ app/controllers/UserControllers.php
@@ -4,15 +4,14 @@ require_once(__DIR__ . '/../models/User.php');
 class UserControllers {
     private $model;
     
-    public function __construct()
+    public function __construct() 
     {
         $this->model = new User();
     }
 
     public function index() {
         $users = $this->model->getAll(); 
-        require_once __DIR__.'/../views/users/index.php';
+        require_once __DIR__ . '/../views/users/index.php';
     }
-
 }
 ?>
--- app/views/users/index.php
+++ app/views/users/index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Product List</title>
     <style>
         .title_table {
@@ -30,13 +31,13 @@
                     <th>email</th>
                     <th>address</th>
                     <th>phone number</th>
-                    <th>creatd at</th>
-                    <th>update at</th>
-                    <th>acction</th>
+                    <th>created at</th>
+                    <th>updated at</th>
+                    <th>action</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($users as $user): ?>
+                <?php foreach ($users as $user): ?>
                     <tr>
                         <td><?= ($user['id']) ?></td>
                         <td><?= ($user['name']) ?></td>
@@ -45,17 +46,20 @@
                         <td><?= ($user['phone_number']) ?></td>
                         <td><?= ($user['created_at']) ?></td>
                         <td><?= ($user['updated_at']) ?></td>
-                        <td><button class="btn btn-warning" onclick="editProduct(this)">Edit</button> <button class="btn btn-danger" onclick="deleteRow(this)">Delete</button></td>
-
+                        <td>
+                            <button class="btn btn-warning" onclick="editProduct(this)">Edit</button>
+                            <button class="btn btn-danger" onclick="deleteRow(this)">Delete</button>
+                        </td>
                     </tr>
-                    <?php endforeach; ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
-
     </div>
 
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
